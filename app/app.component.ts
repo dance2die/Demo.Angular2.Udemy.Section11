9e7be2ffe2beab3f62a8641ed96e9afb0232035f
dance2die <dontbothermewithspam@gmail.com>
2016-07-12 06:43:56 -0400

    componentized navigation bar

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,10 @@
 import {Component} from 'angular2/core';
+import {NavBarComponent} from "./NavBarComponent";
 
 @Component({
     selector: 'my-app',
-    templateUrl: '/app/app.component.html'
+    templateUrl: '/app/app.component.html',
+    directives: [NavBarComponent]
 })
 export class AppComponent {
 }
