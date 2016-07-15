b95c776d5b21e8c77dddbcb6e9587e0e259046af
dance2die <dontbothermewithspam@gmail.com>
2016-07-15 06:53:46 -0400

    added glyphicons for users table

--- app/users.component.ts
+++ app/users.component.ts
@@ -4,11 +4,21 @@ import {UsersService} from './users.service';
 @Component({
     template: `
         <h1>Users</h1>
-        <table class="table table-bordered">
-            <tr *ngFor="#user of users">
-                <td>{{user.name}}</td>
-                <td>{{user.email}}</td>
-            </tr>
+        <table class="table table-bordered" style="table-layout:fixed">
+            <thead>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </thead>
+            <tbody>
+                <tr *ngFor="#user of users">
+                    <td>{{user.name}}</td>
+                    <td>{{user.email}}</td>
+                    <td class="glyphicon glyphicon-edit"></td>
+                    <td class="glyphicon glyphicon-remove"></td>
+                </tr>
+            </tbody>
         </table>
     `,
     providers: [UsersService]
