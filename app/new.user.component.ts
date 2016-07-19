a378674a5d9957452b3a98868ddca00eb7830e55
dance2die <dontbothermewithspam@gmail.com>
2016-07-19 06:43:40 -0400

    Created angular 2 form

--- app/new.user.component.ts
+++ app/new.user.component.ts
@@ -1,9 +1,26 @@
 import {Component} from "angular2/core";
+import {ControlGroup, FormBuilder, Validators} from 'angular2/common';
+import {UserNameValidators} from './username.validators';
 
 @Component({
     selector: 'NewUser',
     templateUrl: 'app/new.user.component.html'
 })
 export class NewUserComponent {
+    private signupForm: ControlGroup;
 
+    constructor(fb: FormBuilder) {
+        this.signupForm = fb.group({
+            name: ['', Validators.compose([
+                Validators.required,
+                UserNameValidators.cannotContainSpace
+            ])],
+            email: [],
+            phone: [],
+            street: [],
+            suite: [],
+            city: [],
+            zipCode: [],
+        });
+    }
 }
---
+++ app/username.validators.ts
@@ -0,0 +1,13 @@
+import {Control} from "angular2/common";
+
+export class UserNameValidators {
+    static cannotContainSpace(control: Control) {
+        // invalid: boolean = false;
+        //
+        // if (invalid) {
+        //     return {cannotContainSpace: true};
+        // }
+
+        return null;
+    }
+}
